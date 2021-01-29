a819d7f24add73e258468adb6ec2080d77ee41b3
rachel-kwak <eunjeong.kwak@nhn.com>
2021-01-30 01:43:42 +0900

    falsy & truthy

---
+++ 09. 타입 변환과 단축 평가/Ex13.js
@@ -0,0 +1,24 @@
+// 114p
+
+// 전달받은 인수가 Falsy 값이면 true, Truthy 값이면 false를 반환한다.
+function isFalsy(v) {
+    return !v;
+}
+
+function isTruthy(v) {
+    return !!v;
+}
+
+// 모두 true를 반환한다. 즉, false로 평가되는 값이다.
+isFalsy(false);
+isFalsy(undefined);
+isFalsy(null);
+isFalsy(0);
+isFalsy(NaN);
+isFalsy('');
+
+// 모두 true를 반환한다. 즉, true로 평가되는 값이다.
+isTruthy(true);
+isTruthy('0'); // 빈 문자열이 아닌 문자열은 Truthy 값이다.
+isTruthy({});
+isTruthy([]);
